62f1be4026c08ba1d97bf75c8c8e146952944b6e
Velasco18 <velasco.osvaldo@yahoo.com.br>
2021-02-19 17:44:06 -0300

    Blog Pessoal

---
+++ src/main/java/com/Generation/Blog_Pessoal/Repository/PostagemRepository.java
@@ -0,0 +1,14 @@
+package com.Generation.Blog_Pessoal.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Generation.Blog_Pessoal.Model.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	
+
+}
